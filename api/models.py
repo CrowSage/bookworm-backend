557de0f253d21d0020be71ee0e2fb94c049fc7f2
Waliimran <waliimran.dev@gmail.com>
2026-03-10 17:52:11 +0500

    Fixing Bugs

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class UserBook(models.Model):
     book_id = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     authors = models.CharField(max_length=255)
-    thumbnail = models.URLField(blank=True)
+    thumbnail = models.URLField(blank=True, null=True)
     status = models.CharField(
         max_length=20, choices=Status.choices, default=Status.WANT_TO_READ
     )
