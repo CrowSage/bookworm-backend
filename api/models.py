d1473ea45b081854166991a33e044790bd1be221
Waliimran <waliimran.dev@gmail.com>
2026-03-10 19:08:59 +0500

    Trying a different solution 3

--- api/models.py
+++ api/models.py
@@ -14,7 +14,7 @@ class UserBook(models.Model):
     book_id = models.CharField(max_length=100)
     title = models.CharField(max_length=255)
     authors = models.CharField(max_length=500)
-    thumbnail = models.URLField(blank=True, null=True)
+    thumbnail = models.URLField(blank=True, null=True, max_length=1000)
     status = models.CharField(
         max_length=20, choices=Status.choices, default=Status.WANT_TO_READ
     )
